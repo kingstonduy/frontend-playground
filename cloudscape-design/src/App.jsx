b8f574b8e8b6979c2fab055b4716127b28d9f2f2
kingstonduy <kingstonduy.duong@gmail.com>
2024-11-14 01:35:10 +0700

    change folder name

---
+++ cloudscape-design/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import AppLayoutPreview from './components/getting_started';
+
+function App() {
+  return (
+    <div>
+      <AppLayoutPreview />
+    </div>
+  );
+}
+
+export default App;
