b08b15c8b16e5813b26c5548b034ac5f128e4aaf
kingstonduy <kingstonduy.duong@gmail.com>
2024-11-19 17:44:38 +0700

    header with dynamic search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 // import Output from "./components/todo-examples/Output";
 // import Output from "./components/header-example/Output";
-import About from "./components/LandingPage";
+// import About from "./components/LandingPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
+import { useState } from "react";
+import SearchBarWithTray from "./components/SearchBarWithTray";
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
     return (
         <>
-            {/* <Output /> */}
-            <About></About>
-            {/* <Output /> */}
+            <BrowserRouter>
+                {/* <div className="w-full min-h-screen bg-white-950">
+                    <Header isLoggedIn={isLoggedIn} />
+                </div>
+
+                <div>
+                    <Footer></Footer>
+                </div> */}
+                <SearchBarWithTray />
+            </BrowserRouter>
         </>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ export default {
             fontFamily: {
                 jost: ['"Jost"', "sans-serif"], // Add Jost as a font option
             },
+            keyframes: {
+                slideUpFade: {
+                    "0%": { transform: "translateY(100%)", opacity: "0" },
+                    "100%": { transform: "translateY(0)", opacity: "1" },
+                },
+                slideDownFade: {
+                    "0%": { transform: "translateY(0)", opacity: "1" },
+                    "100%": { transform: "translateY(100%)", opacity: "0" },
+                },
+            },
+            animation: {
+                slideUpFade: "slideUpFade 0.3s ease-out",
+                slideDownFade: "slideDownFade 0.3s ease-out",
+            },
         },
     },
     plugins: [],
