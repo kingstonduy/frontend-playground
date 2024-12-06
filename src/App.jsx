9eba3a5b46b03538bfd5495c9cec2887ec309511
kingstonduy <kingstonduy.duong@gmail.com>
2024-12-07 01:40:38 +0700

    add base name

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     return (
         <>
-            <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <BrowserRouter basename="/front-end-playground/">
                 {/* <div className="w-full min-h-screen bg-white-950">
                 </div>
 
